f8bea4e331da994df72b1dcbd06649734cb894cf
Amr Kallas <amrkallas1@gmail.com>
2025-03-18 02:09:01 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
